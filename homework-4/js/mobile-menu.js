d52fbd9f38a31a0a28257ad133373b05dda43355
Inna Vulchyn <49243755+Innchonok@users.noreply.github.com>
2024-08-08 19:10:28 -0700

    Add files via upload

--- homework-4/js/mobile-menu.js
+++ homework-4/js/mobile-menu.js
@@ -3,5 +3,12 @@ const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
 
 const toggleMobile_menu = () => mobile_menu.classList.toggle("is-open");
+const disableScroll = () =>
+  document.body.classList.toggle("is-scroll-disabled");
+
 menuBtnOpen.addEventListener("click", toggleMobile_menu);
 menuBtnClose.addEventListener("click", toggleMobile_menu);
+mobile_menu.addEventListener("click", toggleMobile_menu);
+
+menuBtnOpen.addEventListener("click", disableScroll);
+menuBtnClose.addEventListener("click", disableScroll);
